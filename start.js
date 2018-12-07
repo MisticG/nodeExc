6bbcdeea7d613c4a9c2deb436746618c3b173798
tuyvi <tuy-vi.huynh@medieinstitutet.se>
2018-12-07 11:03:45 +0100

    init node

--- start.js
+++ start.js
@@ -4,5 +4,9 @@ var http = require('http');
 
 var server = http.createServer(function (request, response) {
     console.log("Processing Request");
-    response.writeHead(200, {"Content-Type": "text/plain"})
-}
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World\n");
+});
+
+server.listen(8000);
+console.log("Program Ended");
