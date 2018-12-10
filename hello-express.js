8b229b013a47d1cb8e890c4f6507c832d8b9885a
tuyvi <tuy-vi.huynh@medieinstitutet.se>
2018-12-10 13:30:25 +0100

    Hello pug

--- hello-express.js
+++ hello-express.js
@@ -1,10 +1,16 @@
 console.log("Started Program");
 var express = require('express');
+var pug = require('pug');
+
 var app = express();
 
 app.get('/', function (request, response) {
     console.log("Handling Request");
-    response.send('Hello Express');
+    var html = pug.renderFile('page.pug', {
+        'message':'Hello Pug!'
+    });
+
+    response.send(html);
     console.log("Done Handling Process");
 })
 
