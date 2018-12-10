e25d31db1595fb0d74907ab549c3d3a458b62221
tuyvi <tuy-vi.huynh@medieinstitutet.se>
2018-12-10 13:00:45 +0100

    added package.json

--- start.js
+++ start.js
@@ -3,7 +3,7 @@ console.log("Program Started");
 var http = require('http');
 
 var server = http.createServer(function (request, response) {
-    console.log("Processing Request");
+    console.log("Processing Request for URL: " + request.url);
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World\n");
 });
